16404cc0017a943eb2634ebc2cd6409fbde6448f
fatihcalis <icalis@atlassian.com>
2020-10-01 19:26:33 +0300

    vue-slider moved in Slider component, create ControlMenu component

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -1,34 +1,6 @@
 <template>
   <main>
-    <aside>
-      <div class="section-header">
-        <span>Palette settings</span>
-      </div>
-      <div class="side-menu">
-        <div class="side-menu__header">
-          <b>Saturated blue</b>
-          <span class="icon"></span>
-        </div>
-        <div class="side-menu__content">
-          <div class="side-menu__content__settings">
-            <label>Color number</label>
-            <vue-slider :min="0" :max="20" v-model="colorNumber"></vue-slider>
-          </div>
-          <div class="side-menu__content__settings">
-            <label>Hue</label>
-            <vue-slider v-model="hue"></vue-slider>
-          </div>
-          <div class="side-menu__content__settings">
-            <label>Saturation</label>
-            <vue-slider v-model="saturation"></vue-slider>
-          </div>
-          <div class="side-menu__content__settings">
-            <label>Lightness</label>
-            <vue-slider v-model="lightness"></vue-slider>
-          </div>
-        </div>
-      </div>
-    </aside>
+    <ControlMenu></ControlMenu>
     <section>
       <div class="section-header">
         <span>Your color palette</span>
@@ -47,20 +19,18 @@
 
 <script>
 import ColorCard from "../color-card/index";
+import ControlMenu from "../control-menu/index";
 import { mockedPalettes } from "../../mock/index";
 
 export default {
   name: "Main",
   components: {
     ColorCard,
+    ControlMenu,
   },
   data() {
     return {
       palettes: mockedPalettes,
-      colorNumber: 5,
-      hue: 180,
-      saturation: 50,
-      lightness: [20, 80],
     };
   },
 };
