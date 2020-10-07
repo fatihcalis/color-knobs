cffa9df13dd767714a78fc6e7ad2b5bc010878d2
Burak Başcı <59391071+morphine-cake@users.noreply.github.com>
2020-10-07 22:55:12 +0300

    Header styling v01

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,12 +1,39 @@
 <template>
   <header>
-    <div class="header__wrapper"></div>
+    <div class="header__wrapper">
+      <div class="header__wrapper__logo">
+        <img :src="LogoLight" alt="Color Knobs">
+      </div>
+      <div class="header__wrapper__action-box">
+        <div class="header__wrapper__action-box__item">
+          <Night />
+        </div>
+        <div class="header__wrapper__action-box__item">
+          <button class="share-btn">
+            <p>Share</p>
+          </button>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+import LogoLight from '../../assets/logo/logo-light_theme@2x.png'
+import Night from '../../assets/icons/night.svg'
+
 export default {
   name: "Header",
+
+  components: {
+    Night,
+  },
+
+  data: function () {
+    return {
+      LogoLight: LogoLight
+    }
+  }
 };
 </script>
 
