ce45d4e30283152927b8b1bc5da83c4aba4b7c2d
Burak Başcı <59391071+morphine-cake@users.noreply.github.com>
2020-10-22 23:25:43 +0300

    export button added

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -3,7 +3,15 @@
     <Aside></Aside>
     <section>
       <div class="section-header">
-        <span>Your color palette</span>
+        <div class="section-header__title">
+          <span>Your color palette</span>
+        </div>
+        <div class="section-header__action-box">
+           <button class="ghost-btn">
+            <icon name="download" />
+            <p>Export</p>
+          </button>
+        </div>
       </div>
 
       <ColorCard
@@ -21,12 +29,14 @@
 import ColorCard from "../color-card/index";
 import Aside from "../aside/index";
 import { mockedPalettes } from "../../mock/index";
+import Icon from '../shared/icon'
 
 export default {
   name: "Main",
   components: {
     ColorCard,
     Aside,
+    Icon,
   },
   data() {
     return {
